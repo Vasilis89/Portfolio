59fbd6cd4d854d1a7795f8fe3382e6f7e79f0a3d
Vasilis Myrianthopoulos <vasilissubs89@gmail.com>
2023-03-01 21:30:57 -0500

    Fixed project Pictures and Urls

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
-import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
@@ -125,10 +124,6 @@ class App extends Component {
         />
         <Skills
           sharedSkills={this.state.sharedData.skills}
-          resumeBasicInfo={this.state.resumeData.basic_info}
-        />
-        <Experience
-          resumeExperience={this.state.resumeData.experience}
           resumeBasicInfo={this.state.resumeData.basic_info}
         />
         <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
